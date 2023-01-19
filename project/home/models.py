72fe9fd9c1f799547780696b0d8cdfe3562660e1
Anton Khomyk <anton.khomyk@gmail.com>
2023-01-19 06:48:28 +0200

    design

--- project/home/models.py
+++ project/home/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    # title, desc,  image, price
-    title = models.CharField('Name', max_length=120)
+    # slug, title, desc,  image, price
+    slug = models.SlugField('Product')
+    title = models.CharField('Model of Product', max_length=120)
     desc = models.TextField('Description')
     image = models.ImageField('image', default='default.png', upload_to='home_images')
     price = models.CharField('Price', max_length=120)
